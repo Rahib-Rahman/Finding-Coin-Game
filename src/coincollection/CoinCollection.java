5bc0135a1db632b6c8541d99f4f2787c9089812b
Sheikh Md. Rahibur Rahman <61474527+Rahib-Rahman@users.noreply.github.com>
2020-02-26 05:12:47 +0600

    Add files via upload

---
+++ src/coincollection/CoinCollection.java
@@ -0,0 +1,20 @@
+package coincollection;
+
+import com.neub.game.GameFrame;
+
+/**
+ *
+ * @author Sheikh Md. Rahibur Rahman
+ */
+ 
+public class CoinCollection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GameFrame gameFrame = new GameFrame();
+    }
+
+}
